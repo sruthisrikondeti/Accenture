429e4af048c646aa3d60b7032f79fcca17092a1b
Kondeti sruthi sri <sruthisrikondeti298@gmail.com>
2024-09-13 12:13:30 +0530

    Create find_count.py

---
+++ find_count.py
@@ -0,0 +1,35 @@
+'''
+You are given a function,
+int findCount(int arr[], int length, int num, int diff);
+
+The function accepts an integer array ‘arr’, its length and two integer variables ‘num’ and ‘diff’. Implement this function to find and return the number of elements of ‘arr’ having an absolute difference of less than or equal to ‘diff’ with ‘num’.
+Note: In case there is no element in ‘arr’ whose absolute difference with ‘num’ is less than or equal to ‘diff’, return -1.
+
+Example:
+Input:
+
+arr: 12 3 14 56 77 13
+num: 13
+diff: 2
+Output:
+3
+
+Explanation:
+Elements of ‘arr’ having absolute difference of less than or equal to ‘diff’ i.e. 2 with ‘num’ i.e. 13 are 12, 13 and 14.
+'''
+def findCount(n, arr, num, diff):
+    count=0
+    for i in range(n):
+        if(abs(arr[i]-num)<=diff):
+            count+=1
+    if count:
+        return count
+    return 0
+n=int(input())
+arr=[]
+for i in range(n):
+    x=int(input())
+    arr.append(x)
+num=int(input())
+diff=int(input())
+print(findCount(n, arr, num, diff))
