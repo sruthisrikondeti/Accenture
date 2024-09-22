324a315a1396fdb409b97a5b4c16ba909ba702df
Kondeti sruthi sri <sruthisrikondeti298@gmail.com>
2024-09-22 17:05:57 +0530

    Create Nearest_Integer.py

---
+++ Nearest_Integer.py
@@ -0,0 +1,14 @@
+'''Int nearestInteger(int num, int m) 
+The function accepts two positive ‘num’ and ‘m’ as its argument, Implement 
+the following function to find the nearest integer to num. 
+1) Number is divisible by m. 
+2) Number is nearest to ‘num’ (Have the least distance from num) 
+Note: If there are two numbers with the least distance from num, then return 
+the larger num. 
+Input 1: Num= 67 
+M = 8 
+Output 1: 64 
+Input 2: Num=26 
+M=3 
+Output 2: 27
+'''
