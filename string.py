44b723e24a0e640c727b6cd2dfbd97e20331497d
Kondeti sruthi sri <sruthisrikondeti298@gmail.com>
2024-09-24 21:28:43 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,24 @@
+#Q1 - 
+'''check vowel or not'''
+c = 'a'
+
+# checking for vowels
+if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U':
+    print(c, "is a vowel")  # condition true input is vowel
+else:
+    print(c, "is a consonant") 
+#a is a vowel
+'''Q2-Python program to check whether a character is an alphabet or not'''
+# Python Program to find character is alphabet or not
+
+# user input
+ch = 'z'
+
+# basic logic
+if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+    print("The  character", ch, "is an Alphabet")
+else:
+    print("The  character", ch, "is not an Alphabet")
+'''Output
+The  character z is an Alphabet'''
+#Q3
